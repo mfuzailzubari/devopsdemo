13d7a2d52bd672f208591d582f80629d6cd4eacd
Zubari <fuzail.zubari@spglobal.com>
2019-06-26 17:04:43 +0500

    todo app added

--- index.php
+++ index.php
@@ -1,5 +1,63 @@
-<?php
+<?php 
+    // initialize errors variable
+	$errors = "";
 
-echo "Working Fuzail!"
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "todo");
 
-?>
+	// insert a quote if submit button is clicked
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+			$errors = "You must fill in the task";
+		}else{
+			$task = $_POST['task'];
+			$sql = "INSERT INTO tasks (tasks) VALUES ('$task')";
+			mysqli_query($db, $sql);
+			header('location: index.php');
+		}
+	}	
+	?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List Application</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="heading">
+		<h2 style="font-style: 'Hervetica';">ToDo List Application</h2>
+	</div>
+	<form method="post" action="index.php" class="input_form">
+		<?php if (isset($errors)) { ?>
+			<p><?php echo $errors; ?></p>
+		<?php } ?>
+		<input type="text" name="task" class="task_input">
+		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+	</form>
+	<table>
+	<thead>
+		<tr>
+			<th>#</th>
+			<th>Tasks</th>
+			<th style="width: 60px;">Action</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php 
+		// select all tasks if page is visited or refreshed
+		$tasks = mysqli_query($db, "SELECT * FROM tasks");
+
+		while($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $row['id']; ?> </td>
+				<td class="task"> <?php echo $row['tasks']; ?> </td>
+				<td class="delete"> 
+					<a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
+			</tr>
+		<?php } ?>	
+	</tbody>
+</table>
+</body>
+</html>
