d1103306a5dd060e1e16afffa8b5b72ca9ea14e7
Zubari <fuzail.zubari@spglobal.com>
2019-07-03 15:36:58 +0500

    error messages updated

--- test.php
+++ test.php
@@ -2,9 +2,10 @@
 
 require_once('utils.php');
 
-echo 'Hi!';
+echo 'Test Runs Started';
 if(!assert(Utils::getPageTitle() == "Todo List Application"))
 {
+    echo "Test run failed";
     throw new \Exception("Page title is not correct", 1);
 }
 // return 0;
